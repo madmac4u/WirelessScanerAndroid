391df81e6ef9e8b66940b84864a6eeb1431732b1
Mukund <madmac4u@gmail.com>
2021-04-24 14:43:52 +0530

    defined Views inside MainActivity.java

--- app/src/main/java/online/cyberforensic/wirelessscannerandroid/MainActivity.java
+++ app/src/main/java/online/cyberforensic/wirelessscannerandroid/MainActivity.java
@@ -3,12 +3,21 @@ package online.cyberforensic.wirelessscannerandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvWelcomeMessage;
+    Button btnGetAP,btnGetClient,btnGetOther;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvWelcomeMessage = findViewById(R.id.welcomeMessage);
+        btnGetAP = findViewById(R.id.btnAP);
+        btnGetClient = findViewById(R.id.btnClient);
+        btnGetOther = findViewById(R.id.btnOther);
     }
 }
